c1d036213cbe7a54e253a49d689eb15fd13cd06e
codetangle <trevor@devreason.com>
2025-08-05 20:38:25 -0500

    add command line args

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_num_characters, sort_char_count
 
 def get_book_text(filepath):
@@ -12,9 +13,9 @@ def print_character_count(char_count_list):
         if char.isalpha():
             print(f"{char}: {count}")
         
-def print_report(number_words, char_count_list):
+def print_report(book_path, number_words, char_count_list):
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {number_words} total words")
     print("--------- Character Count -------")
@@ -22,12 +23,17 @@ def print_report(number_words, char_count_list):
     print("============= END ===============")
     
 def main():
-    file_path = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+
     book_text = get_book_text(file_path)
     num_words = get_num_words(book_text)
     num_chars = get_num_characters(book_text)
     sorted_char_count = sort_char_count(num_chars)
     
-    print_report(num_words, sorted_char_count)
+    print_report(file_path, num_words, sorted_char_count)
 
 main()
