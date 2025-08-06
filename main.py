945223fa00aefea2b3d3ee2251417a16516a2b98
codetangle <trevor@devreason.com>
2025-08-05 20:22:57 -0500

    Sort char count and print report

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, sort_char_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
+
+def print_character_count(char_count_list):
+    for count_dict in char_count_list:
+        char = count_dict["char"]
+        count = count_dict["num"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+        
+def print_report(number_words, char_count_list):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {number_words} total words")
+    print("--------- Character Count -------")
+    print_character_count(char_count_list)
+    print("============= END ===============")
     
 def main():
     file_path = "./books/frankenstein.txt"
     book_text = get_book_text(file_path)
     num_words = get_num_words(book_text)
     num_chars = get_num_characters(book_text)
-    print(f"{num_words} words found in the document.")
+    sorted_char_count = sort_char_count(num_chars)
     
-    for k, v in num_chars.items():
-        print(f"'{k}': {v}")
+    print_report(num_words, sorted_char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,14 +1,30 @@
 def get_num_words(text):
     words_array = text.split()
+
     return len(words_array)
 
 def get_num_characters(text):
     char_count = {}
     for char in text:
         c = char.lower()
+
         if c in char_count:
             char_count[c] += 1
         else:
             char_count[c] = 1
 
     return char_count
+
+def get_char_count_sort_key(char_count):
+    return char_count["num"]
+
+def sort_char_count(char_dict):
+    list_to_sort = []
+    # convert to list of dicts
+    for k, v in char_dict.items():
+        list_to_sort.append({"char": k, "num": v})
+
+    # sort the list
+    list_to_sort.sort(reverse=True, key=get_char_count_sort_key)
+
+    return list_to_sort
